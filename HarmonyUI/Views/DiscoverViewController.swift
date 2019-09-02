176b7afe1c178d2ced6ef17fa65e23be500eb47c
Mohamed Taher <mohmdtaher96@gmail.com>
2019-09-01 17:49:07 -0700

    new commit

---
+++ HarmonyUI/Views/DiscoverViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DiscoverViewController.swift
+//  HarmonyUI
+//
+//  Created by user on 8/26/19.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
